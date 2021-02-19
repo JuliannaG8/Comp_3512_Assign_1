ce7c7b88bb7fa86690ceb3424af691eb36b1f853
Julianna <gagliardijulianna@gmail.com>
2021-02-19 11:57:37 -0700

    fixed local storage and viewChange bug

--- assign1.js
+++ assign1.js
@@ -10,48 +10,57 @@ document.addEventListener("DOMContentLoaded", async function () {
         try {
             const response = await fetch("http://www.randyconnolly.com/funwebdev/3rd/api/stocks/companies.php");
             storage = await response.json();
+            localStorage.setItem("companies", JSON.stringify(storage));
         } catch (e) {
             console.error(e);
         }
-        localStorage.setItem("companies", storage);
+
         return storage;
     }
     const companies = await loadAndStore();
-    console.log(companies[1]);
+    console.log(companies);
 
-    async function createChart(company){
-        async function retrieveStocks() {
-            const symbol = company.symbol;
-            const response = await fetch(`http://www.randyconnolly.com/funwebdev/3rd/api/stocks/history.php?symbol=${symbol}`);
-            const stocks = await response.json();
-            stocks.sort(((a, b) => a.date < b.date ? -1 : 1));
-            return stocks;
-        }
-        const stockData = await retrieveStocks();
-        console.log(stockData);
-        const dataset = stockData.map(stock => {
-            const color = Math.floor(Math.random()*16777215).toString(16);
-            return {
-                data: [stock.date, stock.open, stock.close, stock.low, stock.high, stock.volume],
-                borderColor: color
-            };
-        });
-        const canvas = document.querySelector("#stockDetails");
-        const stockChart = new Chart(canvas, {
-            type: "line",
-            data: {
-                labels: ["Dates", "Open", "Close", "Low", "High", "Volume"],
-                datasets: dataset
-            },
-            options: {
-                legend: {
-                    display: false
-                }
+    // async function createChart(company){
+    //     async function retrieveStocks() {
+    //         const symbol = company.symbol;
+    //         const response = await fetch(`http://www.randyconnolly.com/funwebdev/3rd/api/stocks/history.php?symbol=${symbol}`);
+    //         const stocks = await response.json();
+    //         stocks.sort(((a, b) => a.date < b.date ? -1 : 1));
+    //         return stocks;
+    //     }
+    //     const stockData = await retrieveStocks();
+    //     console.log(stockData);
+    //     const dataset = stockData.map(stock => {
+    //         const color = Math.floor(Math.random()*16777215).toString(16);
+    //         return {
+    //             data: [stock.date, stock.open, stock.close, stock.low, stock.high, stock.volume],
+    //             borderColor: color
+    //         };
+    //     });
+    //     const canvas = document.querySelector("#stockDetails");
+    //     const stockChart = new Chart(canvas, {
+    //         type: "line",
+    //         data: {
+    //             labels: ["Dates", "Open", "Close", "Low", "High", "Volume"],
+    //             datasets: dataset
+    //         },
+    //         options: {
+    //             legend: {
+    //                 display: false
+    //             }
+    //         }
+    //     });
+    //
+    // }
+    async function retrieveStocks(symbol) {
+                const response = await fetch(`http://www.randyconnolly.com/funwebdev/3rd/api/stocks/history.php?symbol=${symbol}`);
+                const stocks = await response.json();
+                stocks.sort(((a, b) => a.date < b.date ? -1 : 1));
+                return stocks;
             }
-        });
-
+    async function createCharts(company) {
+        const stockData = await retrieveStocks(company.symbol);
     }
-
     function changeViews() {
         document.querySelector(".container").classList.toggle("chartView");
         const view2Elements = document.querySelectorAll(".view2");
@@ -61,5 +70,5 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
     const changeButtons = document.querySelectorAll(".viewChange");
     changeButtons.forEach(button => button.addEventListener('click', changeViews))
-    await createChart(companies[1]);
+    // await createChart(companies[1]);
 });
