3c7178d12b66d3bc04d640ed5acf94751c09771b
Julianna <gagliardijulianna@gmail.com>
2021-02-20 11:50:00 -0700

    added charts and error handling if the selected company doesn't have a financials property

--- assign1.js
+++ assign1.js
@@ -14,61 +14,108 @@ document.addEventListener("DOMContentLoaded", async function () {
         } catch (e) {
             console.error(e);
         }
-
         return storage;
     }
     const companies = await loadAndStore();
     console.log(companies);
 
-    // async function createChart(company){
-    //     async function retrieveStocks() {
-    //         const symbol = company.symbol;
-    //         const response = await fetch(`http://www.randyconnolly.com/funwebdev/3rd/api/stocks/history.php?symbol=${symbol}`);
-    //         const stocks = await response.json();
-    //         stocks.sort(((a, b) => a.date < b.date ? -1 : 1));
-    //         return stocks;
-    //     }
-    //     const stockData = await retrieveStocks();
-    //     console.log(stockData);
-    //     const dataset = stockData.map(stock => {
-    //         const color = Math.floor(Math.random()*16777215).toString(16);
-    //         return {
-    //             data: [stock.date, stock.open, stock.close, stock.low, stock.high, stock.volume],
-    //             borderColor: color
-    //         };
-    //     });
-    //     const canvas = document.querySelector("#stockDetails");
-    //     const stockChart = new Chart(canvas, {
-    //         type: "line",
-    //         data: {
-    //             labels: ["Dates", "Open", "Close", "Low", "High", "Volume"],
-    //             datasets: dataset
-    //         },
-    //         options: {
-    //             legend: {
-    //                 display: false
-    //             }
-    //         }
-    //     });
-    //
-    // }
     async function retrieveStocks(symbol) {
+        function stockStorage () {
+            return JSON.parse(localStorage.getItem(symbol)) || [];
+        }
+        let stocks = stockStorage();
+        if (stocks.length === 0) {
+            try {
                 const response = await fetch(`http://www.randyconnolly.com/funwebdev/3rd/api/stocks/history.php?symbol=${symbol}`);
-                const stocks = await response.json();
+                stocks = await response.json();
                 stocks.sort(((a, b) => a.date < b.date ? -1 : 1));
-                return stocks;
+            } catch (e) {
+                console.error(e);
             }
-    async function createCharts(company) {
+        }
+        localStorage.setItem(symbol, JSON.stringify(stocks));
+        return stocks;
+            }
+            function calculateAverage (data) {
+                let total = 0;
+                data.forEach(stock => total+= Number(stock));
+                console.log(total);
+                return (total / data.length);
+            }
+    async function showData(company) {
         const stockData = await retrieveStocks(company.symbol);
+        const barContainer = document.querySelector("#columns");
+        const view2LineContainer = document.querySelector("#line");
+        const candleContainer = echarts.init(document.querySelector("#candlestick"));
+        const stockTable = document.querySelector("#stockDetails");
+            console.log(company, stockData[1]);
+            const highAvrg = stockData.sort((a, b) => a.high < b.high ? -1 : 1).map(stock => Number(stock.high));
+            const openAvrg = stockData.sort((a, b) => a.open < b.open ? -1 : 1).map(stock => Number(stock.open));
+            const closeAvrg = stockData.sort((a, b) => a.close < b.close ? -1 : 1).map(stock => Number(stock.close));
+            const lowAvrg = stockData.sort((a, b) => a.low < b.low ? -1 : 1).map(stock => Number(stock.low));
+            if (company.hasOwnProperty(financials)) {
+                const barChart = new Chart(barContainer, {
+                type: "bar",
+                data: {
+                    labels: [2019, 2018, 2017],
+                    datasets: [
+                        {
+                            label: "Revenue",
+                            backgroundColor: "#0099cc",
+                            data: company.financials.revenue
+                        },
+                        {
+                            label: "Earnings",
+                            backgroundColor: "#009933",
+                            data: company.financials.earnings
+                        },
+                        {
+                            label: "Assets",
+                            backgroundColor: "#cc9900",
+                            data: company.financials.assets
+                        },
+                        {
+                            label: "Liabilities",
+                            backgroundColor: "#990000",
+                            data: company.financials.liabilities
+                        }
+                    ]
+                }
+            });
+            } else {
+                const errorMsg = `${company.name} does not have stored financial data`;
+                barContainer.getContext("2d").fillText(errorMsg, 1, 50);
+            }
+            const options = {
+                grid: {
+                    height: "80%",
+                    width: "90%",
+                    bottom: "15%"
+                },
+                xAxis: {
+                    data: ['open', 'close', 'high', 'low']
+                },
+                yAxis: {
+                    // data: [30, 60, 90, 120, 150, 180]
+                },
+                series: [{
+                    type: 'candlestick',
+                    data: [
+                        [calculateAverage(openAvrg), calculateAverage(openAvrg), openAvrg[0], openAvrg[openAvrg.length - 1]],
+                        [calculateAverage(closeAvrg), calculateAverage(closeAvrg), closeAvrg[0], closeAvrg[closeAvrg.length - 1]],
+                        [calculateAverage(highAvrg), calculateAverage(highAvrg), highAvrg[0], highAvrg[highAvrg.length - 1]],
+                        [calculateAverage(lowAvrg), calculateAverage(lowAvrg), lowAvrg[0], lowAvrg[lowAvrg.length - 1]]
+                    ]
+                }]
+            };
+            candleContainer.setOption(options);
+
     }
     function changeViews() {
         document.querySelector(".container").classList.toggle("chartView");
-        const view2Elements = document.querySelectorAll(".view2");
-        view2Elements.forEach(element => element.classList.toggle("hidden"));
-        const view1Elements = document.querySelectorAll(".view1");
-        view1Elements.forEach(element => element.classList.toggle("hidden"))
+        document.querySelectorAll(".view2, .view1").forEach(element => element.classList.toggle("hidden"));
     }
-    const changeButtons = document.querySelectorAll(".viewChange");
-    changeButtons.forEach(button => button.addEventListener('click', changeViews))
+    document.querySelectorAll(".viewChange").forEach(button => button.addEventListener('click', changeViews))
     // await createChart(companies[1]);
+    await showData(companies[1]);
 });
